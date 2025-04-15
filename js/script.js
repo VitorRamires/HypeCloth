b7fb6a5448fb72ada6b8024c9961250d7a5efb64
Vitor Ramires <vmr.rs@hotmail.com>
2025-04-15 18:30:10 -0300

    begin responsive

--- js/script.js
+++ js/script.js
@@ -19,3 +19,25 @@ const swiper = new Swiper(".swiper", {
 });
 
 const swiperEl = document.querySelector("swiper-wrapper");
+const allCategoriesLink = document.querySelector(".all-categories-link");
+const submenuCategories = document.querySelector(".all-categories");
+const departmentLink = document.querySelector(".department");
+const departmentCategories = document.querySelector(".department-categories");
+
+allCategoriesLink.addEventListener("mouseover", () => {
+  submenuCategories.classList.add("show-menu");
+  departmentCategories.classList.remove("show-menu");
+});
+
+submenuCategories.addEventListener("mouseleave", () => {
+  submenuCategories.classList.remove("show-menu");
+});
+
+departmentLink.addEventListener("mouseover", () => {
+  departmentCategories.classList.add("show-menu");
+  submenuCategories.classList.remove("show-menu");
+});
+
+departmentCategories.addEventListener("mouseleave", () => {
+  departmentCategories.classList.remove("show-menu");
+});
